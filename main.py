c7270cbfada1927821425a4e3e88778cce1514dc
Ravana <47528708+ravana69@users.noreply.github.com>
2021-04-03 03:53:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -312,8 +312,8 @@ async def tgplay(_, message):
         await send("__**Reply to an audio.**__")
         return
     if message.reply_to_message.audio:
-        if int(message.reply_to_message.audio.file_size) >= 500000000:
-            await send("__**Bruh! Only songs within 500 MB.**__")
+        if int(message.reply_to_message.audio.file_size) >= 104857600:
+            await send("__**Bruh! Only songs within 100 MB.**__")
             playing = False
             return
         duration = message.reply_to_message.audio.duration
