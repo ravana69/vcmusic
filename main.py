4a9323d1af9aad729c0d684b86cad28cb0fe9d3b
Ravana <47528708+ravana69@users.noreply.github.com>
2021-04-04 22:43:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ async def killbot(_, message):
     quit()
 
 
-@app.on_message(filters.command("play"))
+@app.on_message(filters.command("play") & filters.chat(sudo_chat_id))
 async def queuer(_, message):
     usage = "**Usage:**\n__**/play youtube/saavn/deezer Song_Name**__"
     if len(message.command) < 3:
@@ -126,7 +126,7 @@ async def skip(_, message):
     await play()
 
 
-@app.on_message(filters.command("queue"))
+@app.on_message(filters.command("queue") & filters.chat(sudo_chat_id))
 async def queue_list(_, message):
     if len(queue) != 0:
         i = 1
