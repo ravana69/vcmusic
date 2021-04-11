177308907c41221af3c1e576b466c325107b90fa
Ravana <47528708+ravana69@users.noreply.github.com>
2021-04-11 12:48:49 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -6,15 +6,15 @@ if HEROKU:
     API_ID = int(environ["API_ID"])
     API_HASH = environ["API_HASH"]
     SUDO_CHAT_ID = int(environ["SUDO_CHAT_ID"])
-    OWNER_ID = int(environ["OWNER_ID"])
+    SUDOERS = int(environ["SUDOERS"])
     SESSION_STRING = environ["SESSION_STRING"]
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
     API_ID = 14371
     API_HASH = "e46b6c854d2bf58a0"
-    SUDO_CHAT_ID = -1001485876964
-    OWNER_ID = 1243703097
+    SUDO_CHAT_ID = -1001485876964  # Chat where the bot will play the music.
+    SUDOERS = [1243703097, 13216546] # Users which have special control over the bot.
 
 
 # don't make changes below this line
